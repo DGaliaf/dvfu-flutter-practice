b37f4d87cf00b7b989cb099d23b4e791d23c9a41
Danil <106327637+DGaliaf@users.noreply.github.com>
2023-11-01 16:17:34 +1000

    Language switch

--- weather_app/lib/main.dart
+++ weather_app/lib/main.dart
@@ -1,6 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:weather_app/locale_provider.dart';
 import 'package:weather_app/models.dart';
 import 'package:weather_app/service.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+import 'package:provider/provider.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
+
+import 'l10n/all_locales.dart';
 
 void main() {
   runApp(const MyApp());
@@ -11,13 +17,26 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Weather App',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.purple),
-        useMaterial3: true,
-      ),
-      home: const MyHomePage(title: 'Weather App'),
+    return  ChangeNotifierProvider<LocaleProvider>(
+      create: (_) => LocaleProvider(),
+      builder: (context, child) {
+        return MaterialApp(
+          title: 'Weather App',
+          theme: ThemeData(
+            colorScheme: ColorScheme.fromSeed(seedColor: Colors.purple),
+            useMaterial3: true,
+          ),
+          supportedLocales: AllLocales.all,
+          locale: Provider.of<LocaleProvider>(context).locale,
+          localizationsDelegates: const [
+            AppLocalizations.delegate,
+            GlobalMaterialLocalizations.delegate,
+            GlobalCupertinoLocalizations.delegate,
+            GlobalWidgetsLocalizations.delegate,
+          ],
+          home: const MyHomePage(title: 'Weather App'),
+        );    
+      },
     );
   }
 }
@@ -38,7 +57,7 @@ class _MyHomePageState extends State<MyHomePage> {
   WeatherResponse? _response;
 
   void search() async {
-    final response = await dataService.getWeather(cityTextController.text);
+    final response = await dataService.getWeather(cityTextController.text, context);
     
     setState(() => _response = response);
   }
@@ -46,39 +65,70 @@ class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        home: Scaffold(
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
+      home: Scaffold(
+        body: Stack(
           children: [
-            if (_response != null)
-              Column(
-                children: [
-                  Image.network(_response!.iconUrl),
-                  Text(
-                    '${_response!.tempInfo.temperature}°',
-                    style: const TextStyle(fontSize: 40),
+            Stack(
+              children: <Widget>[
+                Align(
+                  alignment: const Alignment(-0.9, 0.9),
+                  child: FloatingActionButton(
+                    child: const Text("EN"),
+                    onPressed: () => {
+                      Provider.of<LocaleProvider>(context,listen: false).setLocale(AllLocales.all[0])
+                    },
                   ),
-                  Text(_response!.weatherInfo.description)
+                ),
+                Align(
+                  alignment: const Alignment(0.9, 0.9),
+                  child: FloatingActionButton(
+                    child: const Text("RU"),
+                    onPressed: () => {
+                      Provider.of<LocaleProvider>(context,listen: false).setLocale(AllLocales.all[1])
+                    },
+                  ),
+                ),
+              ],
+          ),
+          Center(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  if (_response != null)
+                    Column(
+                      children: [
+                        Text(
+                          cityTextController.text,
+                          style: const TextStyle(fontSize: 30),
+                        ),
+                        Text(
+                          '${_response!.tempInfo.temperature}°',
+                          style: const TextStyle(fontSize: 40),
+                        ),
+                        Text(_response!.weatherInfo.description),
+                        Image.network(_response!.iconUrl),
+                      ],
+                    ),
+                  Padding(
+                    padding: const EdgeInsets.symmetric(vertical: 50),
+                    child: SizedBox(
+                      width: 150,
+                      child: TextField(
+                          controller: cityTextController,
+                          decoration: InputDecoration(labelText: AppLocalizations.of(context)!.city.toString()),
+                          textAlign: TextAlign.center),
+                    ),
+                  ),
+                  ElevatedButton(
+                    onPressed: search, 
+                    child: Text(AppLocalizations.of(context)!.search.toString())
+                  )
                 ],
               ),
-            Padding(
-              padding: const EdgeInsets.symmetric(vertical: 50),
-              child: SizedBox(
-                width: 150,
-                child: TextField(
-                    controller: cityTextController,
-                    decoration: const InputDecoration(labelText: 'City'),
-                    textAlign: TextAlign.center),
-              ),
             ),
-            ElevatedButton(
-              onPressed: search, 
-              child: const Text('Search')
-            )
           ],
         ),
       ),
-    ));
+    );
   }
 }
